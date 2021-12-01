20475afc6b55c8da8c92178c1cba0eea97e5056e
MichaelFauht <fauhtm@mymacewan.ca>
2021-12-01 14:46:41 -0700

    Added table to ActorSearch, populate table by search all or containing name. Replaced MovieUI with new version and added back button

---
+++ MovieUI2.designer.cs
@@ -0,0 +1,239 @@
+﻿
+namespace CMPT_291_Project
+{
+    partial class MovieUI2
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.movie = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.genre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.rate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fee = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Select = new System.Windows.Forms.Button();
+            this.MovieInput = new System.Windows.Forms.TextBox();
+            this.Search = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SearchGenre = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Back = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.movie,
+            this.id,
+            this.genre,
+            this.rate,
+            this.fee});
+            this.dataGridView1.Location = new System.Drawing.Point(10, 62);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(560, 221);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // movie
+            // 
+            this.movie.HeaderText = "Movie Name";
+            this.movie.Name = "movie";
+            this.movie.Width = 275;
+            // 
+            // id
+            // 
+            this.id.HeaderText = "Movie ID";
+            this.id.Name = "id";
+            // 
+            // genre
+            // 
+            this.genre.HeaderText = "Genre";
+            this.genre.Name = "genre";
+            // 
+            // rate
+            // 
+            this.rate.HeaderText = "Rating";
+            this.rate.Name = "rate";
+            this.rate.Width = 75;
+            // 
+            // fee
+            // 
+            this.fee.HeaderText = "Fee";
+            this.fee.Name = "fee";
+            this.fee.Width = 75;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Emoji", 12.75F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(10, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select Movie:";
+            // 
+            // Select
+            // 
+            this.Select.Font = new System.Drawing.Font("Segoe UI Emoji", 9F, System.Drawing.FontStyle.Bold);
+            this.Select.Location = new System.Drawing.Point(584, 261);
+            this.Select.Name = "Select";
+            this.Select.Size = new System.Drawing.Size(71, 22);
+            this.Select.TabIndex = 2;
+            this.Select.Text = "Select";
+            this.Select.UseVisualStyleBackColor = true;
+            this.Select.Click += new System.EventHandler(this.Select_Click);
+            // 
+            // MovieInput
+            // 
+            this.MovieInput.Font = new System.Drawing.Font("Segoe UI Emoji", 9F, System.Drawing.FontStyle.Bold);
+            this.MovieInput.Location = new System.Drawing.Point(10, 330);
+            this.MovieInput.Name = "MovieInput";
+            this.MovieInput.Size = new System.Drawing.Size(282, 23);
+            this.MovieInput.TabIndex = 3;
+            // 
+            // Search
+            // 
+            this.Search.Font = new System.Drawing.Font("Segoe UI Emoji", 9F, System.Drawing.FontStyle.Bold);
+            this.Search.Location = new System.Drawing.Point(84, 355);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(117, 23);
+            this.Search.TabIndex = 4;
+            this.Search.Text = "Search Movie";
+            this.Search.UseVisualStyleBackColor = true;
+            this.Search.Click += new System.EventHandler(this.Search_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Comedy",
+            "Action",
+            "Foreign",
+            "Drama"});
+            this.comboBox1.Location = new System.Drawing.Point(334, 330);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(258, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // SearchGenre
+            // 
+            this.SearchGenre.Font = new System.Drawing.Font("Segoe UI Emoji", 9F, System.Drawing.FontStyle.Bold);
+            this.SearchGenre.Location = new System.Drawing.Point(409, 355);
+            this.SearchGenre.Name = "SearchGenre";
+            this.SearchGenre.Size = new System.Drawing.Size(114, 23);
+            this.SearchGenre.TabIndex = 7;
+            this.SearchGenre.Text = "Search Genre";
+            this.SearchGenre.UseVisualStyleBackColor = true;
+            this.SearchGenre.Click += new System.EventHandler(this.SearchGenre_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI Emoji", 9F, System.Drawing.FontStyle.Bold);
+            this.button1.Location = new System.Drawing.Point(584, 236);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(71, 20);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Rent";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Emoji", 12.75F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(147, 296);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(255, 22);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Movie Not Found. Try Again.";
+            // 
+            // Back
+            // 
+            this.Back.Location = new System.Drawing.Point(599, 13);
+            this.Back.Name = "Back";
+            this.Back.Size = new System.Drawing.Size(75, 23);
+            this.Back.TabIndex = 10;
+            this.Back.Text = "Back";
+            this.Back.UseVisualStyleBackColor = true;
+            this.Back.Click += new System.EventHandler(this.Back_Click);
+            // 
+            // MovieUI2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(686, 390);
+            this.Controls.Add(this.Back);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.SearchGenre);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Search);
+            this.Controls.Add(this.MovieInput);
+            this.Controls.Add(this.Select);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "MovieUI2";
+            this.Text = "Movie UI";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn movie;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn genre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn rate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fee;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Select;
+        private System.Windows.Forms.TextBox MovieInput;
+        private System.Windows.Forms.Button Search;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button SearchGenre;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Back;
+    }
+}
+
